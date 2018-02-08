5ecc2aafd933052908f0bda436ed58c71259b758
Shant Wanes <swanes@bjsrestaurants.com>
2018-02-08 15:24:55 -0800

    routing

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+    {
+      name: "Friend1",
+      photo: "https://giphy.com/gifs/running-muppets-7kn27lnYSAE9O",
+      
+      scores: [
+        "1",
+        "1",
+        "1",
+        "2",
+        "3",
+        "1",
+        "2",
+        "1",
+        "1",
+        "1"
+      ]
+    },
+    {
+      name: "IT",
+      photo: "https://giphy.com/gifs/pennywise-it-movie-scary-clown-xT9IgvEOwRzUcZDRiU",
+      scores: [
+        "5",
+        "5",
+        "5",
+        "5",
+        "3",
+        "4",
+        "5",
+        "5",
+        "4",
+        "5"
+      ]
+    },
+    {
+      name: "Baby Boss",
+      photo: "https://giphy.com/gifs/bored-sleepy-boring-LTYT5GTIiAMBa",
+      scores: [
+        "3",
+        "3",
+        "3",
+        "2",
+        "1",
+        "3",
+        "3",
+        "1",
+        "3",
+        "3"
+      ]
+    }
+  ];
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,20 @@
+
+var friends = require("../data/friends");
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+module.exports = function(app) {
+  // API GET Requests
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+  // API POST Requests
+
+  app.post("/api/friends", function(req, res) {
+    res.json(friends);
+  });
+
+};
